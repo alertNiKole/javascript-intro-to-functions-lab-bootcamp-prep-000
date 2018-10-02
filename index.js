731436772f22c2a75e6f6e69a5ef33b849b4f7f4
NiKole Maxwell <alertnikole@gmail.com>
2018-10-02 00:43:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  
+  return string.toUpperCase()
+}
+
+//whisper
+
+function whisper(string){
+  
+  return string.toLowerCase()
+}
+
+//log a shout
+function logShout(string){
+
+  console.log(string.toUpperCase())
+  
+}
+
+function logWhisper(string){
+  
+  console.log(string.toLowerCase())
+}
